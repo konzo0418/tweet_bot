9ed5d62d52a0e1ae26f2bc2d27b16a104599c233
こんぞう <konzo@Konzo-MacBook-Air.local>
2020-05-28 04:23:58 +0900

    ツイート用リスト（list.txt）の追加,Herokuデプロイ用の設定ファイル追加

--- main.rb
+++ main.rb
@@ -2,10 +2,15 @@ require 'twitter'
 
 #作成したTwitterアプリにアクセスする情報を記載する
 client = Twitter::REST::Client.new do |config|
-    config.consumer_key    = "RwZAbLTn9J8l3gfHfFBsltFiA"
-    config.consumer_secret = "4g8CRkszHrjvdRPc5s71iCGTwDtjzu7gSFDPZDXYJsQT0ZMtwc"
-    config.access_token    = "2412453626-BCFdTYAJdPrpEMPgwNydpTu5eglWnqCuc5eUOKB"
-    config.access_token_secret = "HVpZ7PdYJtkE1bnhUbT2cAOqSFukTkSMrOgysiVu2BAXZ"
+  config.consumer_key        = ENV['MY_CONSUMER_KEY']
+  config.consumer_secret     = ENV['MY_CONSUMER_SECRET']
+  config.access_token        = ENV['MY_ACCESS_TOKEN']
+  config.access_token_secret = ENV['MY_ACCESS_TOKEN_SECRET']
 end
 
-client.update("Tweet from Ruby!!!!") #ツイートする文字列
+textlines = Array.new
+File.open("list.txt", mode="rt"){|f|
+  textlines = f.readlines
+}
+
+client.update(textlines.sample) #ツイートする文字列
